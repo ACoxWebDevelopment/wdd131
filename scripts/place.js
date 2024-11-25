df2fe1171f2a5c9078fdb617c8d2dbb4efc536cc
Allan Cox <Allan.L.Coxrr@gmail.com>
2024-11-24 18:33:04 -0700

    week03place

---
+++ scripts/place.js
@@ -0,0 +1,11 @@
+
+function currentyear()
+{
+    let year=new Date().getFullYear();
+    document.getElementById("currentyear").innerText = year;
+}
+
+function lastModified() {
+    let modified= document.lastModified;
+    document.getElementById("lastModified").innerText = `Last Modified: ${modified}`;
+}
