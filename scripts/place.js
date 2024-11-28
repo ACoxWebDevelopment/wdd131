44aa381cdda5d019e441c2f452cd7014da6035f0
Allan Cox <Allan.L.Coxrr@gmail.com>
2024-11-27 22:51:58 -0700

    wind chill

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,3 @@
-
 function currentyear()
 {
     let year=new Date().getFullYear();
@@ -9,5 +8,16 @@ function lastModified() {
     let modified= document.lastModified;
     document.getElementById("lastModified").innerText = `Last Modified: ${modified}`;
 }
+
+function calculateWindChill(temperature, windspeed)
+{
+    if (temperature<=50 && windspeed> 3)
+    {
+        let windchill= (35.74 + 0.6215*temperature-35.75*(windspeed**0.16)+0.4275*temperature*(windspeed**0.16)).toFixed(1);
+        document.getElementById("calculateWindChill").innerText = `Wind Chill: ${windchill}`;
+    }
+    else document.getElementById("calculateWindChill").innerText = `Wind Chill: N/A`;
+}
 currentyear();
 lastModified();
+calculateWindChill(5,10);
